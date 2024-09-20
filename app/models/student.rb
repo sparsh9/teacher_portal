dee5695a8c84c9700163d764cce35efba6469bd8
Sparsh Garg <sparsh@playzap.games>
2024-09-20 19:28:47 +0530

    first commit

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,15 @@
 class Student < ApplicationRecord
   belongs_to :user
+  before_validation :find_or_update_existing_student
+
+  private
+
+  def find_or_update_existing_student
+    existing_student = Student.find_by(name: name, subject: subject)
+
+    if existing_student
+      existing_student.update(marks: existing_student.marks + self.marks)
+      throw(:abort)
+    end
+  end
 end
